ed396e88cf8f84fc4e5a7b2c67f533f0562b4b86
Steve Chaloner <github-NOSPAM@objectify.be>
2013-01-14 09:14:56 +0100

    updated deadbolt version to 2.1 to bring it in line with play

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
     val appName         = "deadbolt-core"
-    val appVersion      = "2.0-SNAPSHOT"
+    val appVersion      = "2.1-SNAPSHOT"
 
     val appDependencies = Seq(
       javaCore
