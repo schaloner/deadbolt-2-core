aa6273cacee1b3e9b7f3c3ba4fbbe98217157a98
schaloner <bitbucket@objectify.be>
2014-01-21 23:07:36 +0100

    Bumped version number

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
     val appName         = "deadbolt-core"
-    val appVersion      = "2.1"
+    val appVersion      = "2.1.1"
 
     val appDependencies = Seq(
       javaCore,
