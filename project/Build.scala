fdeb24937b4d8348b28598177393e65f452b1cb1
Steve Chaloner <bitbucket@objectify.be>
2016-03-31 09:36:23 +0200

    Moved to 2.2.1-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
     val appName         = "deadbolt-core"
-    val appVersion      = "2.2.0"
+    val appVersion      = "2.2.1-SNAPSHOT"
 
     val appDependencies = Seq(
       javaCore,
